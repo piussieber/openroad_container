381da125d23f867a3fd4e0ce6f61c6bce8b71c33
Pius Sieber <pisieber@student.ethz.ch>
2026-03-18 13:51:24 +0100

    fix wrong URL

--- install_openroad.sh
+++ install_openroad.sh
@@ -5,7 +5,7 @@ if test -d OpenROAD; then
     git submodule update --init --recursive
     cd ..
 else
-    git clone --recursive https://github.com/dbekatli/OpenROAD.git
+    git clone --recursive https://github.com/The-OpenROAD-Project/OpenROAD/tree/master
 fi
 
 # build container
